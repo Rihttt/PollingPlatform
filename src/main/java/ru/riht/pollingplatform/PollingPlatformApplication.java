28bad3733560588cc801e0f92afa44f4b2ec8abe
Rihttt <riht1125@gmail.com>
2025-02-24 13:07:47 +0600

    Initial commit

---
+++ src/main/java/ru/riht/pollingplatform/PollingPlatformApplication.java
@@ -0,0 +1,13 @@
+package ru.riht.pollingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollingPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PollingPlatformApplication.class, args);
+    }
+
+}
